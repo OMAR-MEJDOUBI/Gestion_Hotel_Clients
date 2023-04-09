9c39a4da6b1736d3a89c4d707598fb21bb8f53e7
OMAR-MEJDOUBI <omarmejdoubi22@gmail.com>
2023-04-09 06:45:40 -0400

    Implimentation Class Client

--- Mains.cs
+++ Mains.cs
@@ -1,10 +1,18 @@
 ﻿namespace Gestion_Hotel_Clients
 {
-    internal class Program
+    internal class Mains
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hotel hotel1 = new Hotel("Hotel XYZ", "123 rue de la Paix");
+            Client client1 = new Client("Dupont", "Pierre", "pierre.dupont@example.com", "1234 5678 9012 3456", 101);
+            hotel1.AjouterClient(client1);
+
+
+
+            Console.WriteLine(client1.Nom); 
+            Console.WriteLine(client1.NumeroChambre);
+            Console.WriteLine(hotel1.Clients.Count);
         }
     }
 }
